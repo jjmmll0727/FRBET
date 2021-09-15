385b8ee10c3d1f043c13c71e770fecb90daa1b88
jjmmll0727 <jjmmll0727@gmail.com>
2021-09-15 15:05:05 +0900

    change index.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import AxiosApi from './service/axiosApi';
 
-function AxiosApi() {
-    
-  // photos, setPhotos 비구조화 할당
-  let [photos, setPhotos]  = useState<any[]>([]);
+const App: React.FC = () => {
+  return <AxiosApi title ="axios"/>;
+};
 
-  // 통신 메서드
-  function searchApi() {
-      const url = "https://localhos:3300/post/showAll";
-      axios.get(url)
-      .then(function(response) {
-          setPhotos(response.data);
-          console.log("성공");
-      })
-      .catch(function(error) {
-          console.log("실패");
-      })
-      
-  }
-
-  // 조회 데이터 존재할 경우
-  if(photos.length > 0) {
-      return (
-          photos.map(photo => (
-              
-                  <div key={photo.author}>
-                      <p>content : {photo.content}</p>
-                  </div>)
-          )
-      );
-  } else { // 조회 데이터 존재하지 않을 경우
-      return (
-          <div>
-              <button onClick={searchApi}> 불러오기 </button>
-          </div>
-      )
-  }
-}
-export default AxiosApi;
+export default App;
